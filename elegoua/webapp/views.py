034e65351e5a73b0d65c27a695581811f37d5f9e
Alexis Bogroff <31806900+AlexisBogroff@users.noreply.github.com>
2022-12-25 20:46:03 +0100

    Add call to elegoua_engine to define project

--- elegoua/webapp/views.py
+++ elegoua/webapp/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from .forms import ContactForm
 from .views_functions import *
+from elegoua_engine import get_project
 
 def homepage(request):
     return render(request, 'home.html')
@@ -26,10 +27,30 @@ def formpage(request):
             python_level = form.cleaned_data['python_level']
             interest = form.cleaned_data['interest']
 
-            # Make it a list
-            form_data = [first_name, last_name, python_level, interest]
+            # Get student project
+            student_answer = {
+                'level': {'auto_eval_level': int(python_level)},
+                'interests': {
+                    'main_interest': interest,
+                    'other_interests': [],
+                },
+            }
+            project = get_project.Project(student_answer)
+            student_db, student_questions = project.define_project()
+            
+            # Debug print
+            print(f'student_level: {student_db}')
+            print(f'student_questions: {student_questions}')
             
             # Export data to csv
+            form_data = [
+                first_name,
+                last_name,
+                python_level,
+                interest,
+                student_db,
+                student_questions
+            ]
             export_form(form_data)
 
             return render(request, 'thanks.html')
