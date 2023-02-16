f22be9b66042e333f56fef29e12ccebc2f1c066e
MelanieGambiez <gambiez.melanie@gmail.com>
2023-02-16 15:15:32 +0100

    Ajout Page Team

--- elegoua/webapp/urls.py
+++ elegoua/webapp/urls.py
@@ -3,6 +3,7 @@ from . import views
 
 urlpatterns = [
     path('home/', views.homepage, name='home'),
+    path('team/', views.teampage, name='team'),
     path('form/', views.formpage, name='form'),
     path('contact/', views.contactpage, name='contact'),
     path('thanks/', views.thankspage, name='thanks'),
