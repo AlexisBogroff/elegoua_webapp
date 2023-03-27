4bc1f03072a28874355e2e73178bae3c717b1a6f
Tom0103 <elmalehtom@gmail.com>
2023-03-27 15:55:15 +0200

    Edit forms views and thanks

--- elegoua/webapp/views.py
+++ elegoua/webapp/views.py
@@ -1,8 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from elegoua_engine import get_project
 from .forms import ContactForm
 from .views_functions import *
+from elegoua_engine import get_project
 
 
 def homepage(request):
@@ -28,51 +28,76 @@ def formpage(request):
         # Extract and process filled up data        
         form = ContactForm(request.POST)
         if form.is_valid():
-            project=get_project.Project(form.cleaned_data)
-            # Get data from each form field         
-            prenom = form.cleaned_data['prenom']
-            nom = form.cleaned_data['nom']
-            groupe_TD = form.cleaned_data['groupe_TD']
+            # Get data from each form field
+            first_name = form.cleaned_data['first_name']
+            last_name = form.cleaned_data['last_name']
+            group = form.cleaned_data['group']
             email= form.cleaned_data['email']
-            quelles_sont_vos_passions= form.cleaned_data['quelles_sont_vos_passions']
-            dans_quel_domaine_voulez_vous_travailler=form.cleaned_data['dans_quel_domaine_voulez_vous_travailler']
+            interests= form.cleaned_data['interests']
+            work_field=form.cleaned_data['work_field']
             python_level = form.cleaned_data['python_level']
-            quelles_librairies_avez_vous_deja_utilisees=form.cleaned_data["quelles_librairies_avez_vous_deja_utilisees"]
-            quelles_structures_de_donnees_connaissez_vous=form.cleaned_data["quelles_structures_de_donnees_connaissez_vous"]
-            quel_est_le_type_de_données_pour_un_caractere_en_python=form.cleaned_data["quel_est_le_type_de_données_pour_un_caractere_en_python"]
-            quelle_est_la_syntaxe_d_une_boucle_for_pour_parcourir_les_elements_d_une_liste_L= form.cleaned_data["quelle_est_la_syntaxe_d_une_boucle_for_pour_parcourir_les_elements_d_une_liste_L"]
-            quels_sont_les_arguments_de_la_fonction_range=form.cleaned_data["quels_sont_les_arguments_de_la_fonction_range"]
-            quel_est_l_outil_le_plus_rapide_pour_faire_des_calculs_sur_une_matrice=form.cleaned_data['quel_est_l_outil_le_plus_rapide_pour_faire_des_calculs_sur_une_matrice']
-            quelles_sont_vos_activites_extrascolaires=form.cleaned_data['quelles_sont_vos_activites_extrascolaires']
-            quul_metier_envisagez_vous=form.cleaned_data['quel_metier_envisagez_vous']
-            préferez_vous_des_instructions_détailles_ou_des_indications_larges_pour_votre_projet=form.cleaned_data['préferez_vous_des_instructions_détailles_ou_des_indications_larges_pour_votre_projet']
-            qu_est_ce_qu_on_utilise_pour_avoir_acces_a_une_valeur_dans_un_dictionnaire=form.cleaned_data['qu_est_ce_qu_on_utilise_pour_avoir_acces_a_une_valeur_dans_un_dictionnaire']
-            quels_mots_cles_permettent_de_verifier_une_condition=form.cleaned_data['quels_mots_cles_permettent_de_verifier_une_condition']
-            quelle_fonction_permet_de_determiner_la_taille_d_un_dataframe=form.cleaned_data['quelle_fonction_permet_de_determiner_la_taille_d_un_dataframe']
-            quel_argument_permet_d_acceder_aux_attributs_d_un_objet_dans_une_methode_de_classe=form.cleaned_data['quel_argument_permet_d_acceder_aux_attributs_d_un_objet_dans_une_methode_de_classe']
-            quelle_instruction_permet_d_avoir_le_nombre_total_de_valeurs_NA_pour_chaque_variable_d_un_dataframe_df=form.cleaned_data['quelle_instruction_permet_d_avoir_le_nombre_total_de_valeurs_NA_pour_chaque_variable_d_un_dataframe_df']
-            quelle_methode_permet_d_identifier_les_meilleurs_parametres_d_un_modele_ML=form.cleaned_data['quelle_methode_permet_d_identifier_les_meilleurs_parametres_d_un_modèle_ML']
+            libraries_used=form.cleaned_data["libraries_used"]
+            data_structures=form.cleaned_data["data_structures"]
+            data_type_char=form.cleaned_data["data_type_char"]
+            for_loop_syntax= form.cleaned_data["for_loop_syntax"]
+            range_args=form.cleaned_data["range_args"]
+            matrix_tool=form.cleaned_data['matrix_tool']
+            hobbies=form.cleaned_data['hobbies']
+            job=form.cleaned_data['job']
+            level_of_detail=form.cleaned_data['level_of_detail']
+            dico_test=form.cleaned_data['dico_test']
+            condition=form.cleaned_data['condition']
+            lenght=form.cleaned_data['lenght']
+            arg_attributes=form.cleaned_data['arg_attributes']
+            na_values_instruction=form.cleaned_data['na_values_instruction']
+            ml_method=form.cleaned_data['ml_method']
 
-            # Get student project
+            # Define dictionary to map choices to string values
+            level_dico = {'1': 'Beginner', '2': 'Intermediate', '3': 'Advanced'}
+            # Access selected choice and convert to string value
+            python_level = level_dico[python_level]
+            #Get student project
+            interests = interests.split(';')
+            hobbies = hobbies.split(';')
             student_answer = {
-                'level': {'auto_eval_level': int(python_level)},
+                'level': {'auto_eval_level': python_level},
                 'interests': {
-                    'main_interest':[],
-                    'other_interests': [],
+                    'main_interest': interests,
+                    'other_interests': hobbies,
                 },
             }
-            project = get_project.Project(student_answer)
-            student_db, student_questions = project.define_project()            
-            # Debug print
-            print(f'student_level: {student_db}')
-            print(f'student_questions: {student_questions}')
-            # Export data to csv
-            form_data =   []                                                                              
             
+            print(student_answer)
+            project = get_project.Project(student_answer)
+            level,subject = project.define_project()  
+            # Debug print
+            print(f'student_level: {level}')
+            print(f'student_subject: {subject}')
+            # Export data to csv
+            # student_db & student_question
+            form_data = [first_name,last_name,python_level,subject]
             export_form(form_data)
-            return render(request, 'thanks.html')
+            # student_db & student_question
+            # dico = {'dataset_filename' : dataset_filename, 'question':question'}    
+            dico = {"subject":subject,"level" : level}
+            return render(request, 'thanks.html',dico)
     else:
         # Display empty form since the page has just been loaded
         form = ContactForm()
-
+        
     return render(request, 'form.html', {'form': form})
+    
+
+#student_db, student_questions = project.define_project()  
+# Debug print
+# print(f'student_level: {student_db}')
+# print(f'student_questions: {student_questions}')
+# Export data to csv
+# form_data = [
+#     first_name,
+#     last_name,
+#     python_level,
+#     interest,
+#     student_db,
+#     student_questions
+# ]
